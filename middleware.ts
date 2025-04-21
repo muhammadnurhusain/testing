bc51e6ff8d96cd8c35bbf59227ca77edb8021b3b
muhammadnh <muhammadnurhusain60@gmail.com>
2025-04-21 22:20:20 +0700

    fixing middleward akses dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,20 @@
 // middleware.ts
 import { NextRequest, NextResponse } from "next/server";
 
+// export function middleware(req: NextRequest) {
+//   const token = req.cookies.get("token")?.value;
+
+//   const isProtectedRoute = req.nextUrl.pathname.startsWith("/dashboard");
+
+//   if (isProtectedRoute && !token) {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
+
+//   return NextResponse.next();
+// }
+
+// tanpa pengecekan login
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get("token")?.value;
-
-  const isProtectedRoute = req.nextUrl.pathname.startsWith("/dashboard");
-
-  if (isProtectedRoute && !token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
   return NextResponse.next();
 }
 
