dda1175a032a5fc638ea0df9b73ffa9fd6a8ba2e
muhammadnh <muhammadnurhusain60@gmail.com>
2025-04-22 08:42:29 +0700

    setup project nextjs with tailwindcss

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/dashboard/page.tsx
-export default function DashboardPage() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Welcome to the Dashboard</h1>
-      <p className="mt-2 text-gray-600">Pilih menu di sidebar untuk mulai.</p>
-    </div>
-  );
-}
